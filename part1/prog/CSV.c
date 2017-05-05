795ff2bbe84207c5c84f83dceef15844db90490c
Gabriel Toschi <gabriel.toschi@hotmail.com>
2017-05-05 13:57:19 -0300

    Biblioteca CSV, para leitura do arquivo de entrada

---
+++ part1/prog/CSV.c
@@ -0,0 +1,32 @@
+/**
+	CSV
+	Funções que manipulam arquivos CSV para leitura e escrita.
+**/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+	leCSV
+	Le um registro completo de um arquivo, organizado no padrao CSV.
+	PARAMETRO -arquivo- | ponteiro do arquivo CSV
+	RETORNA | vetor de strings, cada uma com o valor de um campo do registro
+**/
+char **leCSV(FILE *arquivo){
+	char **dados;
+	int tamanho;
+	char c;
+
+	dados = (char **) calloc(8,sizeof(char *));
+	for (i = 0; i < 8; i++){ // leia os 8 campos de um registro
+		tamanho = 0;
+		do {
+			c = fgetc(arquivo);
+			dados[i] = (char *) realloc(dados[i],sizeof(char)*(tamanho+1));
+			dados[i][tamanho++] = c;
+		} while(c != ';');
+		dados[i][tamanho-1] = '\0';
+	}
+
+	return dados;
+}
---
+++ part1/prog/CSV.h
@@ -0,0 +1,11 @@
+/**
+	CSV
+	Funções que manipulam arquivos CSV para leitura e escrita.
+**/
+
+#ifndef _CSV_H_
+#define _CSV_H_
+
+char **leCSV(FILE *arquivo);
+
+#endif
