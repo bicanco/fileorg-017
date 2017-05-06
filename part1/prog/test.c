24b543f8547808a4e7f8da8c3f38d5cf2a9e4d12
fertmarana <fernanda.marana44@gmail.com>
2017-05-05 23:55:56 -0300

    Add files via upload

--- part1/prog/test.c
+++ part1/prog/test.c
@@ -4,10 +4,16 @@
 
 #include "CSV.h"
 #include "registro.h"
+#include "indicador.h"
 
 int main (int argc, char *argv[]){
 	FILE *input = fopen("input.csv", "r");
+	FILE *dst = fopen("saida.csv", "w");
+	char * final = criarArquivo_Indicador(input);
+/*
+
 	char **l = leCSV(input);
+	
 	int i;
 
 	for (i = 0; i < 8; i++)
@@ -16,8 +22,11 @@ int main (int argc, char *argv[]){
 
 	int tam;
 	char *r = criaRegistro(l, &tam);
+
 	for (i = 0; i < tam; i++) 
 		printf("%d\t%c\t%d\n", i, r[i], r[i]);
 
 	printf("\n");
+	*/
+		
 }
