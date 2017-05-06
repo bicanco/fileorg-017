2e0e5aa3e0741c1132aac0e968f0b55f75e3fe51
fertmarana <fernanda.marana44@gmail.com>
2017-05-06 15:44:29 -0300

    Update indicador.c

--- part1/prog/indicador.c
+++ part1/prog/indicador.c
@@ -6,13 +6,13 @@
 
 /**
 	CriaRegistro Com Indicador
-	Armazena no arquivo um registro de tamanho variavel com indicador de tamanho,
+	Cria e arruma um registro de tamanho variavel com indicador de tamanho,
 	de acordo com dados recebidos do arquivo CSV e a documentação do trabalho, 
 	quanto ao tamanho, à ordem e a forma com que cada campo é armazenado.
 
 	PARAMETRO -csv- | campos lidos do arquivo CSV
-  PARAMETRO -fds- | endereco apontado pelo ponteiro do arquivo final
- RETORNA |
+    PARAMETRO -tamanhoRegistro- | endereco de um inteiro, para ser retornado o tamanho do registro
+    RETORNA | vetor de bytes, que contém o registro criado
 
 
 **/
@@ -20,9 +20,63 @@
 void criaRegistro_com_indicador(char** csv, FILE *fds){
 	int tamanho;
 	//lendo um registro com campos de tamanhos fixos e campos tamanhos variaveis respectivamente
-	char *registro = criaRegistro(csv, &tamanho);
-	fwrite(&tamanho, sizeof(int), 1, fds);
-	fwrite(registro, sizeof(char), tamanho, fds);
+	char *registro = criaRegistro(csv,&tamanho);
+	fwrite(fds,sizeof(int),1,&tamanho);
+	fwrite(fds,sizeof(char*),1,registro);
 
-	free(registro);
 }
+
+/*
+	BuscaRegistro Com Indicador
+	Le o registro na posicao que o ponteiro do arquivo está apontando.
+	PARAMETRO -fp- | ponteiro do arquivo
+	RETORNA | vetor de bytes, que contém o registro criado
+
+*/
+char *buscaRegistro_com_Indicador(FILE *fp){
+	char *registro= NULL;
+	int tam;
+	//le o tamanho do registro
+	fread(&tam,sizeof(int),1,fp);
+	//aloca memoria e armazena todo registro
+	registro = (char*)realloc(registro,sizeof(char)*tam);
+	fread(registro,sizeof(char*),1,fp);
+	//retorna registro pronto
+	return registro;
+}
+/*
+	BuscaRRN Com Indicador
+	Busca com o indicador de tamanho o rrn desejado.
+	PARAMETRO -fp- | ponteiro do arquivo
+	PARAMETRO -rrn- | rrn desejado
+	RETORNA | vetor de bytes, que contém o registro criado
+
+*/
+char *buscaRRN_Indicador(FILE *fp, int rrn){
+	char *registro= NULL;
+	int tam;
+	int pos = ftell(fp); //vai armazenar a posicao original do fp
+	int i = 0;
+
+	// mandando o fp para o inicio do arquivo
+	fseek(fp,0,SEEK_SET);
+
+	// procurando o rrn
+	while(i!=rrn){
+		fread(&tam,sizeof(int),1,fp);
+		fseek(fp,tam,SEEK_CUR);
+		i++;
+	}
+	// lendo o tamanho do registro
+	fread(&tam,sizeof(int),1,fp);
+	//alocando memoria e guardando o registro numa string
+	registro = (char*)realloc(registro,sizeof(char)*tam);
+	fread(registro,sizeof(char*),1,fp);
+	//vontado fp para a posicao que estava no inicio da funcao
+	fseek(fp,pos,SEEK_SET);
+
+	return registro;
+}
+
+
+
