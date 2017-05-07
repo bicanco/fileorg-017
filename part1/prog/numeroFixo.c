32b070e910d03c8db00729e2f45ef62b94abdc64
niiu <marcelodemoraes11@gmail.com>
2017-05-07 17:01:03 -0300

    busca e escrita- registro com numero de campo fixo

---
+++ part1/prog/numeroFixo.c
@@ -0,0 +1,47 @@
+#include <numeroFixo.h>
+
+void escrever(char* CSV, FILE *fp){
+	//CSV = string de entrada com todos os campos
+	//fp  = FILE* ja aberto atenteriormente, referente ao arquivo a ser escrito
+	int len; 
+
+	//Len = numero de bytes contidos no 
+	len = strlen(CSV);
+	fwrite(CSV, sizeof(char), len, fp);
+}
+
+
+
+
+//   campoFixo1campoFixo2campoFixo4campoFixo4\SIZE\CampoVariado1\SIZE\CampoVariado2\SIZE\CampoVariado3\SIZE\CampoVariado4
+//   \SIZE\ = 4 bytes (int)
+   /*
+	* ler até o fim do registro para determinar seu tamanho
+	* allocar um vetor do tamanho encontrado
+	* dar fseek() para o inicio do registrador 
+	* fread() do fp para o vetor allocado
+	* dar fseek() para o final do registrador
+	* retornar o char* 
+	* obs: a funcao que chama busca() deve, na primeira chamada, mandar a posicao de fp no inicio do FILE* (rewind(fp))
+	*/
+
+char* busca(FILE *fp){
+	char *reg; //registro lido
+	int tamanho, i, tamanhoFixo, tamanhoVariado;
+
+	tamanhoFixo = documentoSize + dataHoraCadastroSize + dataHoraAtualizacaoSize + ticketSize;
+	tamanhoVariado = 0;
+	for(i=0; i<nVariavel; i++){
+		//Le o tamanho do proximo campo e armazena o valor em tamanho;
+		fread(&tamanho, sizeof(int), 1, fp);
+		fseek(fp, tamanho, SEEK_CUR);
+		tamanhoVariado += sizeof(int)+tamanho;
+	}
+
+	reg = (char*)malloc(sizeof(char)*(tamanhoVariado+1)); //+1 por causa do '\0'
+	fseek(fp, -tamanhoVariado, SEEK_CUR); //volta ao inicio do registro
+	fread(reg, sizeof(char), tamanhoVariado, fp);
+	reg[tamanhoVariado] = '\0';
+
+	return reg;
+}
