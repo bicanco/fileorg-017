9a9f34b62b5ff1671abcee6b0c89c4db470b5444
Gabriel Toschi <gabriel.toschi@hotmail.com>
2017-05-09 13:15:00 -0300

    comentarios: caractere.c, caractere.h

--- part1/prog/caractere.c
+++ part1/prog/caractere.c
@@ -1,3 +1,8 @@
+/**
+	caractere
+	Biblioteca com funções auxiliares para o tratamento de texto.
+**/
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -5,62 +10,89 @@
 
 #include "caractere.h"
 
+/**
+	CONSTANTE: INDICA_MINUSC_ACENTO
+	O menor dos valores numéricos de um caractere acentuado,
+	em UTF-8, usado para verificação.
+**/
+#define INDICA_MINUSC_ACENTO -96
+
+/**
+	CONSTANTE: TRANSF_MINUSC_MAIUSC_ACENTO
+	Número que deve ser somado ao valor numérico de um
+	caractere acentuado em UTF-8 para transformá-lo de minúscula
+	para maiúscula.
+**/
+#define TRANSF_MINUSC_MAIUSC_ACENTO -32
+
+/**
+	limpaEntrada
+	Limpa o retentor da entrada padrão (teclado) até uma quebra de linha.
+**/
 void limpaEntrada(){
-	char c;
+	char c; // caractere auxiliar para leitura de caracteres
 	do {
-		c = fgetc(stdin);
-	} while (c != '\n');
+		c = fgetc(stdin); // leia um caractere da entrada padrão
+	} while (c != '\n'); // até que seja lido uma quebra de linha
 }
 
+/**
+	leString
+	Le uma string da entrada padrão até uma quebra de linha.
+
+	RETORNA | endereço de memória da string lida
+**/
 char *leString(){
-	char *str = NULL;
-	char c;
-	int contador = 0;
+	char *str = NULL; // ponteiro para armazenar o endereço da string
+	char c; // caractere auxiliar para leitura de caracteres
+	int contador = 0; // contador de caracteres lidos
 
 	do {
-		c = fgetc(stdin);
+		c = fgetc(stdin); // leia um caractere da entrada padrão
+		// realoque ḿemória para colocar o novo caractere lido
 		str = (char *) realloc(str, sizeof(char) * (contador + 1));
+		// coloque o caractere lido na string, atualize o contador
 		str[contador++] = c;
-	} while (c != '\n');
+	} while (c != '\n'); // até que seja lido uma quebra de linha
 
+	// coloca o indicador de fim de string
 	str[--contador] = '\0';
 	return str;
 }
 
-void stringMaisculaAcentos(char *str){ 
-	int tam = strlen(str); 
+/**
+	stringMaiusculaAcentos
+	Converte todas as letras de uma string para letras maiúsculas,
+	incluindo letras acentuadas.
 
-	int i;
-	for(i=0; i<tam;i++){
-		if (str[i] == INDICA_ACENTO){
-			if (str[++i] >= INDICA_MINUSC_ACENTO)
-				str[i] += TRANSF_MINUSC_MAIUSC_ACENTO;
-		} else if (isalpha(str[i])){
-			str[i] = toupper(str[i]); 
+	PARAMETRO -str- | ponteiro para a string a ser alterada
+**/
+void stringMaiusculaAcentos(char *str){ 
+	int tam = strlen(str); // calcula o tamanho da string
+
+	int i; // variavel para iteração do laço
+	for(i=0; i<tam;i++){ // para cada um dos caracteres da string
+		if (str[i] == INDICA_ACENTO){ // caso ele tenha um byte indicador de acento
+			if (str[++i] >= INDICA_MINUSC_ACENTO) // e o próximo byte seja uma letra acentuada
+				// some o valor de transformação da letra acentuada
+				str[i] += TRANSF_MINUSC_MAIUSC_ACENTO; 
+		} else if (isalpha(str[i])){ // caso seja uma letra não acentuada
+			str[i] = toupper(str[i]); // faça a transformação para letra maiúscula
 		}
 	}
 }
 
-/*
-á : Á | -61, -95 : -61, -127	| -31 : -63
-é : É | -61, -87 : -61, -119	| -23 : -55
-í : Í | -61, -83 : -61, -115	| -19 : -51
-ó : Ó | -61, -77 : -61, -109	| -13 : -45
-ú : Ú | -61, -70 : -61, -102	|  -6 : -38	
+/**
+	acentoWin1252_UTF8
+	Retorna o valor numérico do segundo byte de um caractere acentuado em UTF-8
+	a partir do valor numérico do mesmo caractere acentuado em Windows-1252.
 
-à : À | -61, -96 : -61, -128	| -32 : -64
-
-â : Â |	-61, -94 : -61, -126	| -30 : -62
-ê : Ê |	-61, -86 : -61, -118	| -22 : -54
-ô : Õ | -61, -76 : -61, -108	| -12 : -44
-
-ã : Ã |	-61, -93 : -61, -125	| -29 : -61
-õ : Õ | -61, -75 : -61, -107	| -11 : -43
-
-ç : Ç | -61, -89 : -61, -121	| -25 : -57
-*/
-
+	PARAMETRO -c- | valor numérico do caractere em Windows-1252
+	RETORNA | valor numérico do segundo byte do caractere em UTF-8
+			| OU 0, caso não seja um caractere acentuado
+**/
 char acentoWin1252_UTF8(char c){
+	// letras minúsculas
 	if (c == -31) return -95; // á
 	if (c == -23) return -87; // é
 	if (c == -19) return -83; // í
@@ -78,23 +110,24 @@ char acentoWin1252_UTF8(char c){
 
 	if (c == -25) return -89; // ç
 
-	// MAISCULAS (-32)
-	if (c == -31 -32) return -95 -32; // á
-	if (c == -23 -32) return -87 -32; // é
-	if (c == -19 -32) return -83 -32; // í
-	if (c == -13 -32) return -77 -32; // ó
-	if (c ==  -6 -32) return -70 -32; // ú
+	// LETRAS MAISCÚLAS (valores minúsculos -32)
+	if (c == -31 -32) return -95 -32; // Á
+	if (c == -23 -32) return -87 -32; // É
+	if (c == -19 -32) return -83 -32; // Í
+	if (c == -13 -32) return -77 -32; // Ó
+	if (c ==  -6 -32) return -70 -32; // Ú
 
-	if (c == -32 -32) return -96 -32; // à
+	if (c == -32 -32) return -96 -32; // À
 
-	if (c == -30 -32) return -94 -32; // â
-	if (c == -22 -32) return -86 -32; // ê
-	if (c == -12 -32) return -76 -32; // ô
+	if (c == -30 -32) return -94 -32; // Â
+	if (c == -22 -32) return -86 -32; // Ê
+	if (c == -12 -32) return -76 -32; // Ô
 
-	if (c == -29 -32) return -93 -32; // ã
-	if (c == -11 -32) return -75 -32; // õ
+	if (c == -29 -32) return -93 -32; // Ã
+	if (c == -11 -32) return -75 -32; // Õ
 
-	if (c == -25 -32) return -89 -32; // ç
+	if (c == -25 -32) return -89 -32; // Ç
 
+	// caso não seja um caractere acentuado em Windows-1252, retorne 0
 	return 0;
 }
--- part1/prog/caractere.h
+++ part1/prog/caractere.h
@@ -1,13 +1,21 @@
+/**
+	caractere
+	Biblioteca com funções auxiliares para o tratamento de texto.
+**/
+
 #ifndef _CARACTERE_H_
 #define _CARACTERE_H_
 
+/**
+	CONSTANTE: INDICA_ACENTO
+	O valor numérico do primeiro byte de todos os caracteres
+	acentuados da língua portuguesa em UTF-8.
+**/
 #define INDICA_ACENTO -61
-#define INDICA_MINUSC_ACENTO -96
-#define TRANSF_MINUSC_MAIUSC_ACENTO -32
 
 void limpaEntrada();
 char *leString();
-void stringMaisculaAcentos(char *str);
-char acentoWin1252_UTF8(char c);
+void stringMaiusculaAcentos(char *);
+char acentoWin1252_UTF8(char);
 
 #endif
