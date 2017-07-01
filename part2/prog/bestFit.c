1ae2ed52d4069a222436fc2c57cabf9ff466f63d
Unknown <davidsourod@gmail.com>
2017-07-01 10:42:03 -0300

    bestFit.c

---
+++ part2/prog/bestFit.c
@@ -0,0 +1,41 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+#include"delimitador.h"
+
+void bestFit(FILE* fp, char **csv, int tamanho, int byteoffset, int byteoffset_anterior){
+	int espaco, proximo, aux;
+	char delimitador = DELIMITADOR;
+
+	if(byteoffset == -1){
+		fseek(fp, 0,SEEK_END);
+		insereRegistro_Delimitador(csv, fp);
+		return;
+	}
+
+
+	fseek(fp, byteoffset, SEEK_SET);
+
+	fgetc(fp);
+	fread(&espaco, sizeof(int), 1, fp):
+	fread(&proximo, sizeof(int), 1, fp);
+
+	if( espaco == tamanho){
+		fseek(fp, byteoffset, SEEK_SET);
+		insereRegistro_Delimitador(csv,fp);
+		fseek(fp, byteoffset_anterior+5,SEEK_SET);
+		fwrite(&proximo, sizeof(int),1,fp);
+		return;
+	}else if(espaco > tamanho){
+		aux = espaco-(tamanho+1);
+
+		fseek(fp, byteoffset+1, SEEK_SET);
+		fwrite(&aux,sizeof(int),1,fp);
+		fseek(fp, byteoffset+aux, SEEK_SET);
+		fwrite(delimitador, sizeof(char),1,fp);
+		insereRegistro_Delimitador(csv,fp);
+		return;
+	}else{
+		bestFit(fp, csv, tamanho, proximo, byteoffset);
+	}
+}
