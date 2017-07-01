191ab9b8902c9eb76c1c1f9b3f5b583991416b7e
Unknown <davidsourod@gmail.com>
2017-07-01 09:48:45 -0300

    Correções

--- part2/prog/firstFit.c
+++ part2/prog/firstFit.c
@@ -3,41 +3,39 @@
 
 #include"delimitador.h"
 
-void firstFit(FILE* fp, char **csv, int tamanho, int rrn, int rrn_anterior){
+void firstFit(FILE* fp, char **csv, int tamanho, int byteoffset, int byteoffset_anterior){
 	int espaco, proximo;
 	char delimitador = DELIMITADOR;
 
-	if(rrn == -1){
+	if(byteoffset == -1){
 		fseek(fp, 0,SEEK_END);
 		insereRegistro_Delimitador(csv, fp);
 		return;
 	}
 
 
-	fseek(fp, rrn, SEEK_SET);
+	fseek(fp, byteoffset, SEEK_SET);
 
 	fgetc(fp);
 	fread(&espaco, sizeof(int), 1, fp):
 	fread(&proximo, sizeof(int), 1, fp);
 
 	if( espaço == tamanho){
-		fseek(fp, rrn, SEEK_SET);
+		fseek(fp, byteoffset, SEEK_SET);
 		insereRegistro_Delimitador(csv,fp);
-		fseek(fp, rrn_anterior+5,SEEK_SET);
+		fseek(fp, byteoffset_anterior+5,SEEK_SET);
 		fwrite(&proximo, sizeof(int),1,fp);
 		return;
 	}else if(espaço >= tamanho+10){
 		aux = espaco-(tamanho+1);
 
-		fseek(fp, rrn+1, SEEK_SET);
+		fseek(fp, byteoffset+1, SEEK_SET);
 		fwrite(&aux,sizeof(int),1,fp);
-		fseek(fp, rrn+aux, SEEK_SET);
+		fseek(fp, byteoffset+aux, SEEK_SET);
 		fwrite(delimitador, sizeof(char),1,fp);
 		insereRegistro_Delimitador(csv,fp);
-		fseek(fp, rrn_anterior+5, SEEK_SET);
-		fwrite(&proximo, sizeof(int), 1, fp):
 		return;
 	}else{
-		firstFit(fp, csv, tamanho, proximo, rrn);
+		firstFit(fp, csv, tamanho, proximo, byteoffset);
 	}
 }
