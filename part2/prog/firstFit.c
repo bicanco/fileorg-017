0f8988d4d88576258fceb9fac3c197fbf2c0805e
Unknown <davidsourod@gmail.com>
2017-07-01 15:32:46 -0300

    firstFit.c: inserção de dados

--- part2/prog/firstFit.c
+++ part2/prog/firstFit.c
@@ -20,8 +20,8 @@ int removeRegistro_FirstFit(FILE *arquivo, Indice *indice, int chave){
 	// remocao logica do arquivo de dados
 	fseek(arquivo, offsetRemover, SEEK_SET);
 	fwrite(&indicador, sizeof(char), 1, arquivo);
-	fwrite(&antigoTopo, sizeof(int), 1, arquivo);
 	fwrite(&tamanho, sizeof(int), 1, arquivo);
+	fwrite(&antigoTopo, sizeof(int), 1, arquivo);
 
 	// remocao fisica do arquivo de indice
 	removeIndice(indice, chave);
@@ -29,6 +29,56 @@ int removeRegistro_FirstFit(FILE *arquivo, Indice *indice, int chave){
 	return 1;
 }
 
+void insereRegistro_FirstFit(FILE *arquivo, Indice *indice, char *reg, int tamanho){
+	int topo = retornaTopoArquivo(arquivo);
+	int topo_anterior = -1;
+	int aux;
+	int espaco;
+	char delimitador = DELIMITADOR;
+
+
+	while( topo != -1){
+		fseek(arquivo, topo, SEEK_SET);
+		fgetc(arquivo);
+		fread(&espaco, sizeof(int), 1, arquivo);
+		if(espaco >= tamanho){
+			break;
+		}
+		topo_anterior = topo;
+		fread(&topo, sizeof(int), 1, arquivo);		
+	}
+
+
+
+	if(topo == -1){
+		if(topo_anterior != -1){
+			fseek(arquivo, topo_anterior+5, SEEK_SET);
+			fwrite(&topo, sizeof(int), 1, arquivo);
+		}else{
+			atualizaTopoArquivo(arquivo, topo);
+		}
+		fseek(arquivo, 0, SEEK_END);
+	}else{
+		if((espaco-tamanho) >= 10){
+			fseek(arquivo, topo, SEEK_SET);
+			aux = espaco-tamanho-1;
+			fwrite(&aux, sizeof(int), 1, arquivo);
+			fseek(arquivo, aux-5,SEEK_CUR);
+			fwrite(&delimitador,sizeof(char),1,arquivo);
+		}else if(topo_anterior != -1){
+			fseek(arquivo, topo+5, SEEK_SET);
+			fread(&aux, sizeof(int), 1, arquivo);
+			fseek(arquivo, topo_anterior+5, SEEK_SET);
+			fwrite(&aux, sizeof(int), 1, arquivo);
+		}else{
+			fread(&aux, sizeof(int), 1, arquivo);
+			atualizaTopoArquivo(arquivo, aux);
+		}
+		fseek(arquivo, topo+espaco-tamanho,SEEK_SET);
+	}
+	fwrite(reg, sizeof(char), tamanho, arquivo);
+	fwrite(&delimitador, sizeof(char), 1, arquivo);
+}
 /*
 void firstFit(FILE* fp, char **csv, int tamanho, int byteoffset, int byteoffset_anterior){
 	int espaco, proximo;
